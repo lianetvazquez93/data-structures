55d1e085a28f4f1d03e1ee37052ffb495b5954f4
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-12-03 16:43:20 +0100

    finish pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,43 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+//Solution #1
+// function pyramid(n) {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let level = "";
+
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (col >= midpoint - row && col <= midpoint + row) {
+//         level += "#";
+//       } else {
+//         level += " ";
+//       }
+//     }
+//     console.log(level);
+//   }
+// }
+
+//Solution #2
+function pyramid(n, row = 0, level = "") {
+  const midpoint = Math.floor((2 * n - 1) / 2);
+
+  if (row === n) {
+    return;
+  }
+
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    return pyramid(n, row + 1);
+  }
+
+  if (level.length >= midpoint - row && level.length <= midpoint + row) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+
+  pyramid(n, row, level);
+}
 
 module.exports = pyramid;
