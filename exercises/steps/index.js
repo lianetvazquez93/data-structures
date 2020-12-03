5c7d2c4d6cac595d2afdc7d35edd17490618ae95
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-12-03 15:55:21 +0100

    finish steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,39 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//Solution #1
+// function steps(n) {
+//   for (let i = 0; i < n; i++) {
+//     let step = "";
+//     for (let j = 0; j < n; j++) {
+//       if (j <= i) {
+//         step += "#";
+//       } else {
+//         step += " ";
+//       }
+//     }
+//     console.log(step);
+//   }
+// }
+
+//Solution #2
+function steps(n, row = 0, step = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === step.length) {
+    console.log(step);
+    return steps(n, row + 1);
+  }
+
+  if (step.length <= row) {
+    step += "#";
+  } else {
+    step += " ";
+  }
+
+  steps(n, row, step);
+}
 
 module.exports = steps;
