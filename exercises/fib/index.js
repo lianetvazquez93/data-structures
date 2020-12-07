80b6296c830692426543ec2778fdf1f3c2eff1a3
Lianet Vazquez Fernandez <lianetvazquez93@gmail.com>
2020-12-07 12:24:09 +0100

    finish fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,40 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//Solution #1
+// function fib(n) {
+//   const result = [0, 1];
+
+//   for (let i = 2; i <= n; i++) {
+//     result.push(result[i - 1] + result[i - 2]);
+//   }
+
+//   return result[n];
+// }
+
+function memoize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+//Solution #2
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
